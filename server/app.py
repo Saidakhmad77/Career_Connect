c049fefd3738dfd0c753067cef153e3a0c8e87ba
Saidakhmad77 <lionuz669@gmail.com>
2024-11-12 16:57:14 +0900

    changing port

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -29,4 +30,5 @@ def apply_cors(response):
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
